615cee9b5d22ebea7f59a567823dbc208da65e11
Paola Petrelli <paola.petrelli@utas.edu.au>
2021-12-22 06:57:38 +1100

    added download code and license

---
+++ gpcc_download.sh
@@ -0,0 +1,40 @@
+# Copyright 2021 ARC Centre of Excellence for Climate Extremes 
+#
+# author: Paola Petrelli <paola.petrelli@utas.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script is used to download, checksum and update the GPCC dataset on
+# the NCI server
+# Data is downloaded from the Global Precipitation Climatology Centre data server
+#     https://opendata.dwd.de/climate_environment/GPCC
+# Data is available as monthly (0.25, 0.5, 1.0, 2.5 deg) and daily files (1.0 deg),
+# updated occasionally when a new version is released
+# Current version is v2020
+#
+# The dataset is stored in 
+#     /g/data/ia39/gpcc/replica/data/full_data_<frequency>_<version>/<grid>/<files>
+# where <grid> is the grid resolution 025, 05, 10, 25 respectively for 0.25, 0.5, 1.0, 2.5 degrees
+#
+# To run the script ./gpcc_download.sh 
+# record of updated files is kept in /g/data/ia39/replica/gpcc/data/updates.txt
+#
+# Last change:
+# 2021-12-22
+
+url=https://opendata.dwd.de/climate_environment/GPCC/full_data
+datadir=/g/data/ia39/gpcc/replica/data/full_data
+for grid in 025 05 10 25; do
+    wget -r -np -R "index.html*" -O ${datadir}_monthly_v2020/${grid} ${url}_monthly_v2020/${grid}/
+done
+wget -r -np -R "index.html*" -O ${datadir}_daily_v2020/10 ${url}_daily_v2020/ 
